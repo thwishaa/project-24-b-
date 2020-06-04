ba1156b32ee163a69f8a54d2008d503c843f0a44
thwishaa <64151507+thwishaa@users.noreply.github.com>
2020-06-04 20:09:52 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-24-b-
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ paper.js
@@ -0,0 +1,28 @@
+class paper{
+    constructor (x,y,radius){
+        var options ={
+            isStatic: false,
+            density: 1.2,
+            friction : 0.5,
+            restitution: 0.3
+        }
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius = radius; 
+        World.add(world,this.body);
+9
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("yellow");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius);
+
+        pop();
+    }
+
+    
+}
